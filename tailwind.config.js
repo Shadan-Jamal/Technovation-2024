5e0bcbbb1b36a8cbb864db38eb5c33e3b8a1d3a2
SJ_0741 <112848126+Shadan-Jamal@users.noreply.github.com>
2024-03-21 21:27:48 +0530

    add registration link

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/index.html"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
       'hero-image':"url('./public/assets/hero_section.jpg')"
