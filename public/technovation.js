38c83c8ff894ac6132359bf1c81077cf80e22f33
SJ_0741 <112848126+Shadan-Jamal@users.noreply.github.com>
2024-03-23 19:12:13 +0530

    Add Responsiveness

--- public/technovation.js
+++ public/technovation.js
@@ -3,3 +3,10 @@ var typed = new Typed('#hero-heading', {
     strings: ['TECHNOVATION-2024<br>Events'],
     typeSpeed: 60,
 });
+
+const hero_section=document.getElementById('hero-section').innerW;
+const hero_heading=document.getElementById('hero-heading');
+const events=document.getElementById('events');
+window.addEventListener('scroll',()=>{
+    console.log(window.innerHeight) 
+})
