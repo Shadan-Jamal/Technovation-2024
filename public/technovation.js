7d17526719816654608bd928bb79ce08c7a44179
SJ_0741 <112848126+Shadan-Jamal@users.noreply.github.com>
2024-03-24 20:34:58 +0530

    Add animation to navbar

--- public/technovation.js
+++ public/technovation.js
@@ -10,17 +10,30 @@ const events=document.getElementById('events-nav');
 const navbar=document.getElementById('navbar');
 const btn_nav=document.getElementById('show-nav');
 events.style.display='none';
+
 btn_nav.addEventListener('click',()=>{
     if(!btn_nav.classList.contains('active')){
         btn_nav.style.width='0';
         btn_nav.classList.add('active');
         navbar.insertBefore(events,navbar.children[1]);
-        events.style.display="block";
+        events.style.display="flex";
     }
     else{
-        btn_nav.style.width='44px';
+        btn_nav.style.width='2rem';
         btn_nav.classList.remove('active');
         navbar.removeChild(events);
         events.style.display="none";
     }
 });
+
+window.addEventListener('scroll',()=>{
+    let scrolled=window.scrollY;
+    if(scrolled > 750){
+        navbar.style.visibility="visible";
+        navbar.classList.add('down');
+    }
+    else{
+        navbar.style.visibility="hidden";
+        navbar.classList.remove('down');
+    }
+})
