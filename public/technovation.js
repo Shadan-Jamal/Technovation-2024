190a81440e341b15fe581d9bf358ea87bdfca48d
SJ_0741 <112848126+Shadan-Jamal@users.noreply.github.com>
2024-03-24 13:51:56 +0530

    Add navbar

--- public/technovation.js
+++ public/technovation.js
@@ -4,9 +4,23 @@ var typed = new Typed('#hero-heading', {
     typeSpeed: 60,
 });
 
-const hero_section=document.getElementById('hero-section').innerW;
+const hero_section=document.getElementById('hero-section');
 const hero_heading=document.getElementById('hero-heading');
-const events=document.getElementById('events');
-window.addEventListener('scroll',()=>{
-    console.log(window.innerHeight) 
-})
+const events=document.getElementById('events-nav');
+const navbar=document.getElementById('navbar');
+const btn_nav=document.getElementById('show-nav');
+console.log(btn_nav.classList);
+
+btn_nav.addEventListener('click',()=>{
+    btn_nav.style.transition= "transform .3s linear";
+    if(!btn_nav.classList.contains('active')){
+        btn_nav.style.width='0';
+        btn_nav.classList.add('active');
+        navbar.appendChild(events);
+    }
+    else{
+        btn_nav.style.width='44px';
+        btn_nav.classList.remove('active');
+        navbar.removeChild(events)
+    }
+});
