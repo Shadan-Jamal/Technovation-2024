aca11d0a4d8ae2bfeffd8af4ad9bc979a2137efd
SJ_0741 <112848126+Shadan-Jamal@users.noreply.github.com>
2024-03-26 23:26:33 +0530

    add nav display

--- public/technovation.js
+++ public/technovation.js
@@ -4,24 +4,28 @@ var typed = new Typed('#hero-heading', {
     typeSpeed: 60,
 });
 
-const hero_section=document.getElementById('hero-section');
-const hero_heading=document.getElementById('hero-heading');
 const events=document.getElementById('events-nav');
+const mobile_events=document.getElementById('event-mobile-view');
 const navbar=document.getElementById('navbar');
 const btn_nav=document.getElementById('show-nav');
 events.style.display='none';
+mobile_events.style.display='none';
 
 btn_nav.addEventListener('click',()=>{
     if(!btn_nav.classList.contains('active')){
         btn_nav.style.width='0';
         btn_nav.classList.add('active');
         navbar.insertBefore(events,navbar.children[1]);
+        mobile_events.style.display='block';
+        mobile_events.classList.add="right-0";
+        navbar.insertBefore(mobile_events,navbar.children[2]);
         events.style.display="flex";
     }
     else{
         btn_nav.style.width='2rem';
         btn_nav.classList.remove('active');
         navbar.removeChild(events);
+        navbar.removeChild(mobile_events);
         events.style.display="none";
     }
 });
